78caea78f3eb3c0bcb52626a79cd80608c7d03ab
vanderleiot <central@hostbdc.com.br>
2017-03-23 22:58:25 -0300

    comida

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://comidaajato.com/mobileapp/api",
+	'DialogDefaultTitle' : "Comidaajato",
+	'pushNotificationSenderid' : "1093334082077",
+	'facebookAppId' : "698431266981651",
+	'APIHasKey' : ""
+};
